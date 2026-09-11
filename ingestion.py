9e459127848e853b69af1c95591767076e605f48
Bern4rdR <rumarbernard@gmail.com>
2026-09-11 22:43:51 +0200

    compartmentalize ingestion and transform

--- ingestion.py
+++ ingestion.py
@@ -1,5 +1,9 @@
+from typing import Any
+
 from delta import *
 from pathlib import Path
+
+from pyspark.sql.dataframe import DataFrame
 from custom_builder import builder
 from log import *
 
@@ -48,99 +52,99 @@ from pyspark.sql import functions as F
 import json
 from pathlib import Path
 
-
-for config_file in Path("ingestion_configuration").glob("*.json"):
-    
-    with log_step("ingest_data") as info:
-        # 1. Load the configuration and dataframe
-        with open(config_file) as f:
-            config = json.load(f)
+def ingest(conf) -> tuple[DataFrame, Any]:
+    # 1. Load the configuration and dataframe
+    with open(conf) as f:
+        config = json.load(f)
 
 
-        if config["source"].endswith(".parquet"):
-            df = spark.read \
-                .option("header", True) \
-                .option("inferSchema", False) \
-                .parquet(config["source"])
-        else:
-            df = spark.read \
-                .option("header", True) \
-                .option("inferSchema", False) \
-                .csv(config["source"])
-        
-    # ideally we would perform validation here
-    # atm, we do ingestion and transformation before validation
+    if config["source"].endswith(".parquet"):
+        df = spark.read \
+            .option("header", True) \
+            .option("inferSchema", False) \
+            .parquet(config["source"])
+    else:
+        df = spark.read \
+            .option("header", True) \
+            .option("inferSchema", False) \
+            .csv(config["source"])
 
-    with log_step("transform_data") as info:
-        
-        # 2. Build the selected/transformed columns
-        selected_columns = []
+    return df, config
 
-        for column in config["columns"]:
-            source = column["source"]
-            name = column["name"]
-            data_type = column["type"]
+def transform(df, config) -> DataFrame:
+	# 2. Build the selected/transformed columns
+    selected_columns = []
 
-            if source is None:
-                continue
+    for column in config["columns"]:
+        source = column["source"]
+        name = column["name"]
+        data_type = column["type"]
 
-            if isinstance(source, list):
-                if data_type == "timestamp":
-                    if config["source"] == f"{DB_SRC}/air_quality/hourly_88101_2024.csv":
-                        expr = F.to_timestamp(
-                            F.concat_ws(" ", *[F.col(c) for c in source]),
-                            "yyyy-MM-dd HH:mm"
-                        ).alias(name)
-                    elif config["source"] == f"{DB_SRC}/weather.csv":
-                        expr = F.to_timestamp(
+        if source is None:
+            continue
+
+        if isinstance(source, list):
+            if data_type == "timestamp":
+                if config["source"] == f"{DB_SRC}/air_quality/hourly_88101_2024.csv":
+                    expr = F.to_timestamp(
+                        F.concat_ws(" ", *[F.col(c) for c in source]),
+                        "yyyy-MM-dd HH:mm"
+                    ).alias(name)
+                elif config["source"] == f"{DB_SRC}/weather.csv":
+                    expr = F.to_timestamp(
+                        F.concat_ws(
+                            " ",
                             F.concat_ws(
-                                " ",
-                                F.concat_ws(
-                                    "-",
-                                    F.col(source[0]),
-                                    F.lpad(F.col(source[1]), 2, "0"),
-                                    F.lpad(F.col(source[2]), 2, "0")
-                                ),
-                                F.concat(
-                                    F.lpad(F.col(source[3]), 2, "0"),
-                                    F.lit(":00")
-                                )
+                                "-",
+                                F.col(source[0]),
+                                F.lpad(F.col(source[1]), 2, "0"),
+                                F.lpad(F.col(source[2]), 2, "0")
                             ),
-                            "yyyy-MM-dd HH:mm"
-                        ).alias(name)
-                else:
-                    raise ValueError(
-                        f"Multiple source columns are only handled for timestamp, "
-                        f"got type={data_type}"
-                    )
-
+                            F.concat(
+                                F.lpad(F.col(source[3]), 2, "0"),
+                                F.lit(":00")
+                            )
+                        ),
+                        "yyyy-MM-dd HH:mm"
+                    ).alias(name)
             else:
-                expr = F.col(source)
+                raise ValueError(
+                    f"Multiple source columns are only handled for timestamp, "
+                    f"got type={data_type}"
+                )
 
-                # Apply configured type
-                if data_type == "string":
-                    expr = expr.cast("string")
-                elif data_type == "float":
-                    expr = expr.cast("float")
-                elif data_type == "int":
-                    expr = expr.cast("int")
-                elif data_type == "timestamp":
-                    expr = F.to_timestamp(expr)
+        else:
+            expr = F.col(source)
 
-                expr = expr.alias(name)
+            # Apply configured type
+            if data_type == "string":
+                expr = expr.cast("string")
+            elif data_type == "float":
+                expr = expr.cast("float")
+            elif data_type == "int":
+                expr = expr.cast("int")
+            elif data_type == "timestamp":
+                expr = F.to_timestamp(expr)
 
-            selected_columns.append(expr)
+            expr = expr.alias(name)
 
-        # print(f"Selected columns: {[c._jc.toString() for c in selected_columns]}")
-        result_df = df.select(*selected_columns)
+        selected_columns.append(expr)
 
-    result_df.printSchema()
-    result_df.show(truncate=False)          
+    # print(f"Selected columns: {[c._jc.toString() for c in selected_columns]}")
+    return df.select(*selected_columns)
 
-    with log_step("write_delta") as info:
-    
-        # persist delta table
-        result_df.write.format("delta").mode("overwrite").saveAsTable(f"{config['name']}")
+
+if __name__ == "__main__":
+	for config_file in Path("ingestion_configuration").glob("*.json"):
+		df, config = ingest(config_file)
+		result_df = transform(df, config)
+
+		result_df.printSchema()
+		result_df.show(truncate=False)
+
+		with log_step("write_delta"):
+	        # persist delta table
+			result_df.write.format("delta").mode("overwrite").saveAsTable(f"{config['name']}")
 
 
 # df = read_source(spark, config["source"])
